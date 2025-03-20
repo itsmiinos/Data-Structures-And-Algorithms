f1b929c7638ce3bc4ab9e17c3b252ec755872368
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-03-20 23:11:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.js
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split("\n").map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+
+        let input_arr = readLine().split(' ').map(x => parseInt(x));
+        let arr = new Array(input_arr.length);
+        for (let i = 0; i < input_arr.length; i++) {
+            arr[i] = input_arr[i];
+        }
+
+        let obj = new Solution();
+        let res = obj.largest(arr);
+        console.log(res);
+        console.log("~");
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    /**
+    * @param number[] arr
+
+    * @returns number
+    */
+    largest(arr) {
+        // code here
+        let max = arr[0];
+        for(let i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
