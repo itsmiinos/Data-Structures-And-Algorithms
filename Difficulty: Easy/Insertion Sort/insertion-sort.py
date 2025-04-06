6133eba073f48ef963516c5bc08da6f370db765c
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-04-06 21:51:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.py
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -9,13 +9,12 @@
 class Solution:
     def insertionSort(self, arr):
         #code here
-        for i in range (1,len(arr)) : 
-            for j in range (i , 0 , -1) :
+        for i in range(1,len(arr)) :
+            for j in range (i , 0, -1) :
                 if arr[j-1] > arr[j] :
                     [arr[j] , arr[j-1]] = [arr[j-1] , arr[j]]
                 
         return arr
-
 
 #{ 
  # Driver Code Starts.
