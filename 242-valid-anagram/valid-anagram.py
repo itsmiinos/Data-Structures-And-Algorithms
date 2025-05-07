6ccc2c8118994a075fb07c6a67bb33f1084a5cc9
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-05-07 21:52:48 +0530

    Time: 16 ms (25.20%) | Memory: 18 MB (32.74%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        my_dict_s = {}
+        my_dict_t = {}
+
+        for i in range (0,len(s)):
+            my_dict_s[s[i]] = my_dict_s.get(s[i] , 0) + 1
+        
+        for i in range (0,len(t)):
+            my_dict_t[t[i]] = my_dict_t.get(t[i], 0) + 1
+
+        if len(my_dict_s)!=len(my_dict_t) :
+            return False
+        
+        for key in my_dict_s.keys() :
+            if my_dict_s.get(key) != my_dict_t.get(key):
+                return False
+        
+        return True
