f2d3158731f93211d5aad9c471d28b3a21809bc4
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-07 13:16:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.py
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.py
@@ -3,20 +3,21 @@
 class Solution:
     def knapSack(self, val, wt,capacity):
         # code here
-        dp = [[-1 for j in range(capacity+1)] for i in range(len(val) + 1)]
-        result = self.knapsackHelper(val , wt , capacity , len(val)-1 , dp)
+        dp = [[-1 for j in range (capacity+1)] for i in range (len(val))]
+        result = self.knapsackHelper(val , wt , len(val)-1 , capacity , dp)
         return result
         
-    def knapsackHelper(self , val : [int] , wt : [int] , capacity : int , i:int , dp : list[list[int]]) -> int : 
-        if i < 0 or capacity == 0 : 
+    def knapsackHelper(self , val : [int] , wt:[int] , i : int , W : int , dp : list[list[int]]) -> int : 
+        
+        if i < 0 : 
             return 0
+            
+        if dp[i][W] != -1 : return dp[i][W]
         
-        if dp[i][capacity] != -1 : return dp[i][capacity]
-        
-        a = self.knapsackHelper(val , wt , capacity , i-1 , dp)
-        b = -1 
-        if wt[i] <= capacity :
-            b = self.knapsackHelper(val , wt , capacity - wt[i] , i , dp) + val[i]
-        
-        dp[i][capacity] = max(a , b)
-        return dp[i][capacity]
+        temp1 = self.knapsackHelper(val , wt , i-1 , W , dp)
+        temp2 = -1
+        if wt[i] <=W : 
+            temp2 = self.knapsackHelper(val , wt , i , W-wt[i] , dp) + val[i]
+            
+        dp[i][W] = max(temp1 , temp2)
+        return dp[i][W]
