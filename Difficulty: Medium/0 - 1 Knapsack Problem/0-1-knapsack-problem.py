9f6f3ac62ef9d8c4bb0cc67224968b297016c2bd
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-07 13:08:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -1,22 +1,21 @@
 class Solution:
     def knapsack(self, W, val, wt):
-        n = len(val)
-        dp = [[-1 for _ in range(W + 1)] for _ in range(n)]
-        return self.knapsackHelper(W, val, wt, n - 1, dp)
+        # code here
+        dp = [[-1 for j in range (W+1)] for i in range (len(val))]
+        result = self.knapsackHelper(W , val , wt , len(val)-1 , dp)
+        return result
         
-    def knapsackHelper(self, W, val, wt, i, dp):
-        if i < 0 or W == 0:
+    def knapsackHelper(self , W : int , val : [int] , wt : [int] , i : int , dp : list[list[int]]) -> int : 
+        
+        if i<0 : 
             return 0
-        if dp[i][W] != -1:
-            return dp[i][W]
-
-        # Exclude current item
-        not_pick = self.knapsackHelper(W, val, wt, i - 1, dp)
-
-        # Include current item if it fits
-        pick = -1
-        if wt[i] <= W:
-            pick = val[i] + self.knapsackHelper(W - wt[i], val, wt, i - 1, dp)
-
-        dp[i][W] = max(pick, not_pick)
-        return dp[i][W]
+        if dp[i][W] !=-1 : return dp[i][W]
+        
+        temp1 = self.knapsackHelper(W , val , wt , i-1 , dp) #exclude
+        temp2 = -1
+        if wt[i] <= W : 
+            temp2 = self.knapsackHelper(W-wt[i] , val , wt , i-1 , dp) + val[i] #include
+            
+        dp[i][W] = max(temp1 , temp2)
+        
+        return max(temp1 , temp2)
