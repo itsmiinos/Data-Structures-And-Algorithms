16e0edaf01a935316b72eda77a6de49a94a89a46
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-09 23:59:55 +0530

    Time: 33 ms (49.49%) | Memory: 23.7 MB (88.84%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.py
@@ -1,17 +1,18 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        ans = 0
-        last_greater_element_index = 0
-        previous_valid_starting_points = 0
+        sum = 0
+        last_greater_element = 0 
+        previous_starting_points = 0
 
-        for ep in range(len(nums)) :
-            if nums[ep] > right :
-                last_greater_element_index = ep + 1
-                previous_valid_starting_points = 0
-            if nums[ep] >=left and nums[ep] <=right :
-                ans += ep - last_greater_element_index + 1
-                previous_valid_starting_points = ep - last_greater_element_index + 1
-            if nums[ep] < left:
-                ans += previous_valid_starting_points
-            
-        return ans
+        for i in range (len(nums)) : 
+
+            if nums[i] < left : 
+                sum += previous_starting_points
+            if nums[i] > right : 
+                last_greater_element = i+1
+                previous_starting_points = 0
+            if nums[i] >=left and nums[i] <=right :
+                previous_starting_points = i - last_greater_element + 1
+                sum += i - last_greater_element + 1
+
+        return sum
