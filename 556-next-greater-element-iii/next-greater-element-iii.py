0d1560c8bc37dce5bb5b77a187ed8dcab2e9c841
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-11 23:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.99%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.py
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -1,32 +1,47 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-        nums = [int(d) for d in str(n)]
+        temp = n
+        nums = []
+
+        while temp>0 : 
+            res = temp%10
+            nums.append(res)
+            temp = temp//10
+        
+        self.reverseArray(nums , 0 , len(nums)-1)
         dip_index = -1
-
-        # Step 1: Find dip
-        for i in range(len(nums) - 1, 0, -1):
-            if nums[i - 1] < nums[i]:
-                dip_index = i - 1
+        for i in range (len(nums)-1 , 0 , -1) : 
+            if nums[i-1] < nums[i] :
+                dip_index = i-1
                 break
 
-        if dip_index == -1:
-            return -1  # No next permutation
-
-        # Step 2: Find just greater element to the right of dip
-        for i in range(len(nums) - 1, dip_index, -1):
-            if nums[i] > nums[dip_index]:
-                nums[i], nums[dip_index] = nums[dip_index], nums[i]
+        if dip_index < 0 : 
+            return -1
+        
+        next_greater_element = 0
+        for i in range (len(nums)-1 , dip_index , -1) : 
+            if nums[i] > nums[dip_index] : 
+                next_greater_element = i
                 break
+        
+        [nums[next_greater_element] , nums[dip_index]] = [nums[dip_index] , nums[next_greater_element]]
 
-        # Step 3: Reverse the suffix
-        left = dip_index + 1
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        self.reverseArray(nums , dip_index+1 , len(nums)-1)
+        
+        number = 0
+        for i in range(len(nums)) : 
+            number = (number * 10) + nums[i]
 
-        # Step 4: Convert back to number and check limit
-        number = 0; 
-        for d in nums: number = (number * 10) + d
-        return number if number < 2**31 else -1
+        if number >= 2**31 :
+            return -1
+        
+        if number == n : 
+            return -1
+        
+        return number
+
+    def reverseArray(self , nums : [int] , left : int , right : int) : 
+        while left<=right : 
+            [nums[left] , nums[right]] = [nums[right] , nums[left]]
+            left+=1
+            right-=1
