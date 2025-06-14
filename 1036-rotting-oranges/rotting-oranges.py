a217a33d1af27c87d089961485d4d328e46696d8
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-14 21:02:42 +0530

    Time: 3 ms (82.24%) | Memory: 17.8 MB (44.78%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,45 +1,33 @@
-class Pair : 
-    row = 0
-    col = 0
-    t = 0
-    def __init__(self , i : int , j : int , t : int) :
-        self.row = i
-        self.col = j
-        self.t = t 
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        my_queue = []
         n = len(grid)
         m = len(grid[0])
-        my_queue = []
         ans = 0
 
         for i in range (n) : 
             for j in range (m) : 
                 if grid[i][j] == 2 : 
                     my_queue.append(Pair(i , j , 0))
-        
-        while len(my_queue) > 0 : 
+
+        while len(my_queue) > 0  :
             temp = my_queue.pop(0)
             crow = temp.row
             ccol = temp.col
-            ans = temp.t
+            ans = temp.time
 
             if crow-1 >= 0 and grid[crow-1][ccol] == 1 : 
-                my_queue.append(Pair(crow-1 , ccol , temp.t+1))
+                my_queue.append(Pair(crow-1 , ccol , ans+1))
                 grid[crow-1][ccol] = 2
-            
-            if crow+1 < len(grid) and grid[crow+1][ccol] == 1 : 
-                my_queue.append(Pair(crow+1 , ccol , temp.t+1))
+            if crow+1 <= n-1 and grid[crow+1][ccol] == 1 : 
+                my_queue.append(Pair(crow+1 , ccol , ans+1)) 
                 grid[crow+1][ccol] = 2
-            
-            if ccol+1 < len(grid[0]) and grid[crow][ccol+1] == 1 : 
-                my_queue.append(Pair(crow , ccol+1 , temp.t+1))
-                grid[crow][ccol+1] = 2
-            
-            if ccol-1 >=0 and grid[crow][ccol-1] == 1 : 
-                my_queue.append(Pair(crow , ccol-1 , temp.t+1))
+            if ccol-1 >= 0 and grid[crow][ccol-1] == 1 :
+                my_queue.append(Pair(crow , ccol-1 , ans+1))
                 grid[crow][ccol-1] = 2
+            if ccol+1 <= m-1 and grid[crow][ccol+1] == 1 : 
+                my_queue.append(Pair(crow , ccol + 1 , ans+1))
+                grid[crow][ccol+1] = 2
             
         for i in range (n) : 
             for j in range (m) : 
@@ -47,4 +35,14 @@ class Solution:
                     return -1
         
         return ans
-            
+
+
+class Pair : 
+    row = 0
+    col = 0
+    time = 0
+
+    def __init__(self, r , c , t) : 
+        self.row = r
+        self.col = c
+        self.time = t
