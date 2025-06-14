e4cb10e252a96c65eedc5c28c76ab2ea74559d2d
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-14 20:46:02 +0530

    Time: 1031 ms (7.55%) | Memory: 83.5 MB (94.35%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,27 +1,30 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        my_queue = []
-        if n < 2 : 
-            return True
-        my_queue.append(source)
-        visited = [False]*(n+1)
-        adj = self.constructGraph(n , edges)
-        while len(my_queue) > 0 :
-            node = my_queue.pop(0)
-            
-            for neighbour in adj[node] :
-                if visited[neighbour] == False : 
-                    my_queue.append(neighbour)
-                    visited[neighbour] = True
-        return visited[destination]
-    
-    def constructGraph(self , n : int , edges : List[List[int]]) -> List[List[int]] :
+        adj = self.convertToAdjMatrix(n , edges)
         
-        adj = [[] for _ in range(n)]
+        visited = [False]*(len(adj))
+        my_queue = []
+        my_queue.append(adj[0])
+        visited[0] = True
 
-        for u, v in edges:
+        while len(my_queue) > 0 : 
+            neighbours = my_queue.pop(0)
+
+            for n in neighbours : 
+                if visited[n] != True : 
+                    my_queue.append(adj[n])
+                    visited[n] = True
+        
+        return visited[source] and visited[destination]
+    
+    def convertToAdjMatrix(self , n : int , edges : List[List[int]]) -> List[List[int]]: 
+        adj = []
+
+        for i in range (n) : 
+            adj.append([]) #intializing list of list
+        
+        for u,v in edges : 
             adj[u].append(v)
             adj[v].append(u)
         
         return adj
-        
