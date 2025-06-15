72ef81ead274ac62f606542ea700febea9d0db0a
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-15 14:13:23 +0530

    Time: 9 ms (18.26%) | Memory: 19.1 MB (61.53%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,44 +1,41 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0]*numCourses
-        visited = [False]*numCourses
+        adj = self.convertToAdj(numCourses , prerequisites)
+
+        indegree = [0]*(numCourses)
+        visited = [False]*(numCourses)
         for u,v in prerequisites : 
-            indegree[v] +=1
+            indegree[u] +=1
         
-        graph = self.constructGraph(numCourses , prerequisites)
-
         my_queue = []
-
-        for i in range (len(indegree)) : 
-            if indegree[i] == 0 : 
+        for i in range(numCourses) : 
+            if indegree[i] == 0 :
                 my_queue.append(i)
         
-        while len(my_queue) > 0 :
+        while len(my_queue) > 0 : 
             temp = my_queue.pop(0)
             visited[temp] = True
-            neighbours = graph[temp]
+            neighbours = adj[temp]
 
             for n in neighbours : 
-                indegree[n] -=1
+                indegree[n] -=1 
                 if indegree[n] == 0 :
                     my_queue.append(n)
         
         for i in range (len(visited)) : 
             if visited[i] == False : 
                 return False
-
+        
         return True
 
-
-
     
-    def constructGraph(self, num: int , prerequisites : list[list[int]]) -> list[list[int]] : 
-        adj = []
+    def convertToAdj(self , numCourses : int , prerequisites : list[list[int]]) -> list[list[int]] :
+        result = []
 
-        for i in range (num) : 
-            adj.append([])
-        
-        for u , v in prerequisites : 
-            adj[u].append(v)
+        for i in range(numCourses) : 
+            result.append([])
 
-        return adj
+        for u,v in prerequisites : 
+            result[v].append(u)
+
+        return result
