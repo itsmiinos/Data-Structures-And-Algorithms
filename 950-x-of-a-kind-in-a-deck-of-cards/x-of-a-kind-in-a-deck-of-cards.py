5a9ed70fafd2302416319d1b9f590b655bc90bcb
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-17 21:29:42 +0530

    Time: 7 ms (21.39%) | Memory: 18.3 MB (18.83%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        feq_map = {}
+
+        for i in range(len(deck)) : 
+            feq_map[deck[i]] = feq_map.get(deck[i] , 0) + 1 
+        
+        ans = 0
+        for key in feq_map.keys() : 
+            ans = self.gcdCalculator(ans , feq_map[key])
+            if ans <=1 : 
+                return False
+        
+        return True
+    
+    def gcdCalculator(self , a:int , b:int) -> int : 
+        if a == 0 : 
+            return b
+        
+        temp = self.gcdCalculator(b%a , a)
+        return temp
