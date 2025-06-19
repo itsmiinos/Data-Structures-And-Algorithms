d2c727fdefc5bafc84f4d2b05bd6700a4a43344e
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-19 15:01:53 +0530

    Time: 6 ms (29.41%) | Memory: 18.5 MB (90.15%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pointer1 = 0
+        pointer2 = len(nums)-1
+
+        while pointer1 < pointer2 : 
+
+            if nums[pointer1] + nums[pointer2] == target : 
+                return [pointer1+1 , pointer2+1] 
+
+            elif nums[pointer1] + nums[pointer2] < target : 
+                pointer1 +=1
+            
+            else : 
+                pointer2 -=1
+        
+        return [pointer1+1 , pointer2+1]
