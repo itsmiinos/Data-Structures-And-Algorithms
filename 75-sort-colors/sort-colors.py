ea05875d303e6313dc40b9dd77c0500de04d4763
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-19 15:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.71%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,21 +1,22 @@
 class Solution:
-    def sortColors(self, nums):
+    def sortColors(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         i = 0
         j = 0
-        k = len(nums) - 1
-
-        while j <= k:
-            if nums[j] == 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j += 1
-            elif nums[j] == 2:
-                nums[k], nums[j] = nums[j], nums[k]
-                k -= 1
-                # Don't move j here — check new value at nums[j] in next loop
-            else:
-                j += 1  # nums[j] == 1
-
-        return nums
-    
+        k = len(arr)-1
+        
+        while j <= k : 
+            if arr[j] == 0 :
+                [arr[i] , arr[j]] = [arr[j] , arr[i]]
+                j+=1
+                i+=1
+            
+            elif arr[j] == 1 : 
+                j+=1
+            
+            elif arr[j] == 2 : 
+                [arr[j] , arr[k]] = [arr[k] , arr[j]]
+                k-=1
         
