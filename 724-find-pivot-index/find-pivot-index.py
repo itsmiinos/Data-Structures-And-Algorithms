8f6262a4950ba27b20fdba19e04897cb9ae7c096
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-22 13:58:46 +0530

    Time: 37 ms (14.07%) | Memory: 19.1 MB (10.31%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,16 +1,22 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        prefixSum = [-1]*len(nums)
+        runningSum = [None]*len(nums)
+        runningSum[0] = nums[0]
 
-        prefixSum[0] = nums[0]
+        for i in range(1, len(nums)) : 
+            runningSum[i] = runningSum[i-1] + nums[i]
 
-        for i in range(1 , len(nums)) :
-            prefixSum[i] = prefixSum[i-1] + nums[i]
-        
-        for i in range(0 , len(nums)) :
-            leftSum = prefixSum[i - 1] if i > 0 else 0
-            rightSum = prefixSum[len(nums)-1] - prefixSum[i]
+        for i in range(len(nums)) : 
+            if i == 0 : 
+                leftSum = 0 
+                rightSum = runningSum[len(nums)-1] - runningSum[i]
+            elif i == len(nums)-1 :
+                leftSum = runningSum[i-1] 
+                rightSum = 0
+            else : 
+                leftSum = runningSum[i-1]
+                rightSum = runningSum[len(nums)-1] - runningSum[i]
+            print(leftSum , rightSum)
             if leftSum == rightSum : 
                 return i
-
         return -1
