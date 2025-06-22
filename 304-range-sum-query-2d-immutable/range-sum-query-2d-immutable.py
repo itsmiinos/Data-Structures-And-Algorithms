eeef29dd44036a13b324f9ac3e01fd1719e69cf2
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-22 15:34:32 +0530

    Time: 132 ms (30.68%) | Memory: 30.6 MB (90.23%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,37 +1,36 @@
 class NumMatrix:
 
-    prefixSum = []
     def __init__(self, matrix: List[List[int]]):
+        self.prefixSum = [[-1 for j in range(len(matrix[0]))] for i in range(len(matrix))]
         n = len(matrix)
         m = len(matrix[0])
-        self.prefixSum = [[0 for j in range (m)] for i in range(n)]
-        
-        for i in range (n) : 
-            self.prefixSum[i][0] = matrix[i][0]
 
-        for i in range (n) :
-            for j in range (1, m) : 
+        for i in range(n) : 
+            self.prefixSum[i][0] = matrix[i][0]
+        
+        for i in range(n) : 
+            for j in range(1, m) : 
                 self.prefixSum[i][j] = self.prefixSum[i][j-1] + matrix[i][j]
         
-        for j in range (m) : 
-            for i in range (1 , n) : 
+        for j in range(m) : 
+            for i in range(1,n) : 
                 self.prefixSum[i][j] = self.prefixSum[i-1][j] + self.prefixSum[i][j]
-
 
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         sum = self.prefixSum[row2][col2]
 
-        if row1-1 >=0 : 
+        if row1-1 >= 0 : 
             sum = sum - self.prefixSum[row1-1][col2]
 
-        if col1-1 >=0 : 
+        if col1-1 >= 0 : 
             sum = sum - self.prefixSum[row2][col1-1]
         
-        if row1-1 >=0 and col1-1>=0 : 
+        if row1-1 >= 0 and col1-1 >= 0 : 
             sum = sum + self.prefixSum[row1-1][col1-1]
-
+        
         return sum
+        
 
 
 # Your NumMatrix object will be instantiated and called as such:
