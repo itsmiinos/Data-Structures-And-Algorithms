51fc4e0b911173c19443465648c24db16d4411c4
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-23 16:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.82%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.py
+++ 556-next-greater-element-iii/next-greater-element-iii.py
@@ -1,47 +1,53 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
         temp = n
-        nums = []
-
-        while temp>0 : 
-            res = temp%10
-            nums.append(res)
-            temp = temp//10
+        dip_index = 0
         
-        self.reverseArray(nums , 0 , len(nums)-1)
+        nums = self.convertNumberToArrayAndReverse(n)
+
+        n = len(nums)
         dip_index = -1
-        for i in range (len(nums)-1 , 0 , -1) : 
-            if nums[i-1] < nums[i] :
-                dip_index = i-1
+        for i in range(n-2 , -1 , -1) : 
+            if nums[i] < nums[i+1] : 
+                dip_index = i
                 break
 
-        if dip_index < 0 : 
+        if dip_index == -1 : 
             return -1
         
-        next_greater_element = 0
-        for i in range (len(nums)-1 , dip_index , -1) : 
+        next_greater_digit_index = 0
+        for i in range(n-1 , dip_index , -1) : 
             if nums[i] > nums[dip_index] : 
-                next_greater_element = i
+                next_greater_digit_index = i
                 break
         
-        [nums[next_greater_element] , nums[dip_index]] = [nums[dip_index] , nums[next_greater_element]]
+        [nums[dip_index] , nums[next_greater_digit_index]] = [nums[next_greater_digit_index] , nums[dip_index]]
 
-        self.reverseArray(nums , dip_index+1 , len(nums)-1)
+        self.reversePartOfArray(nums, dip_index+1 , n-1)
+        n = self.convertArrayToNumber(nums)
+
+        if n >= 2**31 :
+            return -1
         
+        return n
+    
+    def convertNumberToArrayAndReverse(self , n : int) -> [int] : 
+        return [int(d) for d in str(n)]
+
+    def reversePartOfArray(self , nums : [int] , start : int , end: int) : 
+        while(start < end) : 
+            [nums[start] , nums[end]] = [nums[end] , nums[start]]
+            start+=1
+            end-=1
+    
+    def convertArrayToNumber(self , nums : [int]) -> int : 
         number = 0
-        for i in range(len(nums)) : 
-            number = (number * 10) + nums[i]
 
-        if number >= 2**31 :
-            return -1
-        
-        if number == n : 
-            return -1
+        for i in range(len(nums)) : 
+            number = number*10 + nums[i]
         
         return number
 
-    def reverseArray(self , nums : [int] , left : int , right : int) : 
-        while left<=right : 
-            [nums[left] , nums[right]] = [nums[right] , nums[left]]
-            left+=1
-            right-=1
+
+
+
