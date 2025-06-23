ca9c99ad92ebfa8eedfd79b32104e991f464c160
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-23 16:09:39 +0530

    Time: 2 ms (92.61%) | Memory: 20.4 MB (70.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,40 +1,35 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        value1 = nums[0]
-        count1 = 1
-        value2 = nums[0]
-        count2 = 0
+        n = len(nums)
+        val1 = nums[0]
+        count1=1
+        val2 = nums[0]
+        count2=0
 
-        for i in range (1, len(nums)):
-            if nums[i] == value1 : 
+        for i in range(1,n) : 
+            if nums[i] == val1 : 
                 count1+=1
-            elif nums[i] == value2 : 
+            
+            elif nums[i] == val2 : 
                 count2+=1
-            
-            elif count1 == 0 :
-                count1 = 1
-                value1 = nums[i]
-
-            elif count2 == 0 : 
+            elif count1==0:
+                val1 = nums[i]
+                count1=1
+            elif count2==0 : 
+                val2 = nums[i]
                 count2 = 1
-                value2 = nums[i]
-            
-            elif nums[i]!= value1 and nums[i]!=value2 : 
+
+            elif nums[i]!=val1 and nums[i]!=val2 : 
                 count1-=1
                 count2-=1
-            
-        cnt1 = 0
-        cnt2 = 0
-        ans = []
-        for i in range(len(nums)) : 
-            if nums[i] == value1 : 
-                cnt1 +=1
-            elif nums[i] == value2 : 
-                cnt2 +=1
         
-        if cnt1 > len(nums)//3 : 
-            ans.append(value1)
-        if cnt2 > len(nums)//3 : 
-            ans.append(value2)
         
-        return ans
+        result = []
+        c1=0
+        c2=0
+        if nums.count(val1) > n//3 : 
+            result.append(val1)
+        if val1!=val2 and nums.count(val2) > n//3 : 
+            result.append(val2)
+
+        return result
