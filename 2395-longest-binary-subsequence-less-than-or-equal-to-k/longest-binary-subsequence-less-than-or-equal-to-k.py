f8f2f6307122b6a01800b61cb99932df930515cc
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-27 00:11:30 +0530

    Time: 3 ms (69.92%) | Memory: 17.7 MB (66.67%) - LeetSync

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        length = 0
+        power = 1
+        for i in reversed(s) : 
+            if i == '0' : 
+                length +=1
+            else : 
+                if power <= k and int(i) * power <= k : 
+                    k = k - int(i) * power
+                    length+=1
+            if power<= k : 
+                power = power * 2
+        
+        return length
+
+              
