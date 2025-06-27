6cedb388a192670120301f387431b1bc3f50af28
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-27 20:31:53 +0530

    Time: 547 ms (10.05%) | Memory: 18.1 MB (77.19%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,39 +1,32 @@
+import sys
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        if len(s) < len(t) : 
-            return ""
-
         map_t = {}
+        map_s = {}
 
         for i in range(len(t)) : 
             map_t[t[i]] = map_t.get(t[i] , 0) + 1
         
         start = 0
-        window = {}
-        min_string = ""
-        min_length = float('inf')
+        ans_string = ""
+        minimum_window_length = sys.maxsize
+        for i in range(len(s)) : 
+            map_s[s[i]] = map_s.get(s[i] , 0) + 1
 
-        for index in range(len(s)) : 
-
-            window[s[index]] = window.get(s[index] , 0) + 1
-
-            while self.hasElements(window , map_t) :
-                if (index - start + 1) < min_length : 
-                    min_length = index - start + 1
-                    min_string = s[start : index+1]
-                
-                window[s[start]] = window.get(s[start] , 0) - 1
-                if window[s[start]] == 0 :
-                    del window[s[start]]
+            while self.hasElements(map_s , map_t) : 
+                if i - start + 1 < minimum_window_length : 
+                    minimum_window_length = i - start + 1
+                    ans_string = s[start : i+1]
+                map_s[s[start]] = map_s.get(s[start] , 0) - 1
+                if map_s[s[start]] == 0 : 
+                    del map_s[s[start]]
                 start+=1
-            
-        return min_string
-        
 
-    
-    def hasElements(self , map1 : {str : int} , map2 : {str , int}) -> bool :
-        for key in map2 : 
-            if key not in map1 or map1[key] < map2[key] : 
+        return ans_string
+ 
+        
+    def hasElements(self , map1 : {str , int} , map2 : {str : int}) -> bool : 
+        for u in map2 : 
+            if u not in map1 or map1[u]<map2[u] : 
                 return False
         return True
