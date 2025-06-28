291dcc2e360b7db90d6c5643952e83704eba2292
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-28 15:56:02 +0530

    Time: 151 ms (5.13%) | Memory: 34.6 MB (97.75%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -0,0 +1,16 @@
+import heapq
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        heap = []
+        rank = 1
+
+        for i in range(len(arr)) : 
+            heapq.heappush(heap , (arr[i] , i))
+        
+        while len(heap) > 0 : 
+            element , index = heapq.heappop(heap)
+            arr[index] = rank
+            if heap and heap[0][0] > element : 
+                rank+=1
+        
+        return arr
