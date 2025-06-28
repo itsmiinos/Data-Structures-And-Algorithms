cc7f8325a58290d4b66dc762ab9ea37094160782
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-06-28 14:08:53 +0530

    Time: 158 ms (41.90%) | Memory: 39.7 MB (43.61%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -2,30 +2,26 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.my_min_heap = []
-        self.my_max_heap = []
-        self.data_count = 0
+        self.max_heap = []
+        self.min_heap = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.my_max_heap, -num)
-
-        # balance by pushing the largest from small to large
-        heapq.heappush(self.my_min_heap, -heapq.heappop(self.my_max_heap))
-
-        # maintain size property: len(small) >= len(large)
-        if len(self.my_max_heap) < len(self.my_min_heap):
-            heapq.heappush(self.my_max_heap, -heapq.heappop(self.my_min_heap))
-        self.data_count+=1
+        if len(self.max_heap) == len(self.min_heap) : 
+            #first insert into min_heap then pop the min_heap to insert to max_heap
+            heapq.heappush(self.min_heap , num)
+            temp = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap , -temp)
+        else : 
+            #first we insert into max_heap then pop the max_heap to insert to min_heap
+            heapq.heappush(self.max_heap , -num)
+            temp = -heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap , temp)
 
     def findMedian(self) -> float:
-        if self.data_count % 2 == 0 : 
-            left_maximum = -self.my_max_heap[0]
-            right_minimum = self.my_min_heap[0]
-            median = float((left_maximum + right_minimum) / 2.0)
-            return median
+        if len(self.max_heap) > len(self.min_heap) : 
+            return -self.max_heap[0]
         else : 
-            return float(-self.my_max_heap[0])
-
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
