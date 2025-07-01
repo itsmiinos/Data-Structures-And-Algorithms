eb31804a45292cba512c7b9c7d8ba7f4e5c80a88
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-02 02:55:09 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (97.65%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,28 +1,38 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        myStack = []
+        operand_stack = []
 
-        for i in tokens :
-            if i == '+' :
-                val1 = myStack.pop()
-                val2 = myStack.pop()
-                sum = val1 + val2
-                myStack.append(sum)
-            elif i == '-' :
-                val2 = myStack.pop()
-                val1 = myStack.pop()
-                minus = val1 - val2
-                myStack.append(minus)
-            elif i == '*' : 
-                val2 = myStack.pop()
-                val1 = myStack.pop()
-                product = val1 * val2
-                myStack.append(product)
-            elif i == '/' :
-                val2 = myStack.pop()
-                val1 = myStack.pop()
-                remainder = int(val1 / val2)
-                myStack.append(remainder)
+        i = 0
+        while i<len(tokens) : 
+            if tokens[i] in ['+' , '-' , '*' , '/'] :  
+                value2 = operand_stack.pop(-1)
+                value1 = operand_stack.pop(-1)
+                operator = tokens[i]
+                ans = self.calculate(value1 , value2 , operator)
+
+                operand_stack.append(ans)
             else : 
-                myStack.append(int(i))
-        return myStack[0]
+                operand_stack.append(int(tokens[i]))
+            # print(operand_stack)
+            i+=1
+        
+        
+        return operand_stack[-1]
+    
+
+    def calculate(self , value1 : int , value2 : int, operator : str) -> int : 
+        if operator == '+' : 
+            return value1 + value2
+        elif operator == '-' : 
+            return value1 - value2
+        elif operator == '*' : 
+            return value1 * value2
+        else : 
+            return int(value1 / value2)
+    
+    def precedence(self , operator : str) -> int :
+        if operator == '*' or operator == '/' : 
+            return 2
+        elif operator == '+' or operator == '-' : 
+            return 1
+        return 0
