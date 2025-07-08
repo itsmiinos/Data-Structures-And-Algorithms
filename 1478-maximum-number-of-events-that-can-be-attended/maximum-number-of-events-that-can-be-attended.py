e9ec9822d21d8c97f338bc588f0e0f59593d3a13
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-08 14:21:46 +0530

    Time: 123 ms (89.50%) | Memory: 53.3 MB (31.33%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,25 +1,26 @@
+import heapq
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        # Sort events based on start day
-        events.sort()
-        
-        min_heap = []
-        day = 0
-        index = 0
+
+        events.sort(key = lambda event : event[0])
         n = len(events)
-        result = 0
-        
-        while min_heap or index < n:
-            if not min_heap:
-                day = events[index][0]
-            while index < n and events[index][0] <= day:
-                heapq.heappush(min_heap, events[index][1])
-                index += 1
-            heapq.heappop(min_heap)
-            result += 1
-            day += 1
+        day = 0
+        i = 0
+        count = 0
+        heap = []
+
+        while len(heap) > 0 or i < n : 
+            if not heap :
+                day = events[i][0]
             
-            while min_heap and min_heap[0] < day:
-                heapq.heappop(min_heap)
+            while i < n and events[i][0] <= day:
+                heapq.heappush(heap, events[i][1])
+                i+=1
+
+            heapq.heappop(heap)
+            count+=1
+            day+=1
         
-        return result
+            while len(heap) > 0 and heap[0] < day : 
+                heapq.heappop(heap)
+        return count
