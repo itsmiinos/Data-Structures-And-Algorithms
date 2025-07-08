d8baf8aa312d3c1582bbbb114923fb1b5f83633d
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-08 23:52:39 +0530

    Time: 42 ms (51.73%) | Memory: 18.4 MB (84.24%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -11,31 +11,33 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         temp = head
 
+        if not head : 
+            return None
+
         while temp is not None : 
-            copy_node = Node(temp.val)
-            copy_node.next = temp.next
+            copy_node_val = temp.val
+            copy_node = Node(copy_node_val)
+
+            next_node = temp.next
             temp.next = copy_node
+            copy_node.next = next_node
             temp = temp.next.next
-        
+
         temp = head
-        
-        while temp is not None : 
-            copy_node = temp.next
-            if temp.random is None : 
-                copy_node.random = None
-            else :
-                copy_node.random = temp.random.next
+        while temp:
+            if temp.random:
+                temp.next.random = temp.random.next
             temp = temp.next.next
+
+        copy_head = head.next
+        temp1 = head
+        temp2 = copy_head
+
+        while temp1:
+            temp1.next = temp2.next
+            temp1 = temp1.next
+            if temp1:
+                temp2.next = temp1.next
+            temp2 = temp2.next
         
-        dummyNode = Node(-1)
-        temp = head
-        copy_temp = dummyNode
-
-        while temp is not None : 
-            copy_temp.next = temp.next
-            temp.next = temp.next.next
-            copy_temp = copy_temp.next
-            temp = temp.next
-
-        return dummyNode.next
-            
+        return copy_head
