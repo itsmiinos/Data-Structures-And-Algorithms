e2225f092e7c546a31b3e47cb5e17f22c628db0e
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-15 19:27:03 +0530

    Time: 422 ms (5.00%) | Memory: 40.2 MB (5.18%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,57 +1,46 @@
-import sys
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        nse_on_right = self.nextSmallerElementOnRight(heights)
-        nse_on_left = self.nextSmallerElementOnLeft(heights)
+        left_smallest_element = [-1]*len(heights)
+        right_smallest_element = [-1]*len(heights)
 
-        max_area = -sys.maxsize
+        left_stack = []
+        right_stack = []
 
+        for i in range(len(heights) - 1 , -1 , -1) : 
+            while len(left_stack) > 0 and heights[i] < left_stack[-1][0] : 
+                popped = left_stack.pop(-1)
+                popped_value = popped[0]
+                popped_index = popped[1]
+
+                left_smallest_element[popped_index] = i
+            
+            left_stack.append([heights[i] , i])
+        
+        while len(left_stack) > 0 : 
+            popped = left_stack.pop(-1)
+            popped_index = popped[1]
+
+            left_smallest_element[popped_index] = -1
+        
         for i in range(len(heights)) : 
+            while len(right_stack) > 0 and heights[i] < right_stack[-1][0] : 
+                popped = right_stack.pop(-1)
+                popped_value = popped[0]
+                popped_index = popped[1]
+
+                right_smallest_element[popped_index] = i
             
-            nse_on_right_index = nse_on_right[i] - 1
-            nse_on_left_index = nse_on_left[i] + 1
+            right_stack.append([heights[i] , i])
+        
+        while len(right_stack) > 0 : 
+            popped = right_stack.pop(-1)
+            popped_index = popped[1]
 
-            area = heights[i] * (nse_on_right_index - nse_on_left_index + 1)
-            max_area = max(area , max_area)  
+            right_smallest_element[popped_index] = len(heights)
 
+        max_area = -float('inf')
+        for i in range(len(heights)) : 
+            area = heights[i] * ((right_smallest_element[i]-1) - (left_smallest_element[i]+1) + 1)
+            max_area = max(area , max_area)
+        
         return max_area
-
-    def nextSmallerElementOnRight(self , heights : [int]) -> [int] : 
-        stack = []
-        result= [-1]*len(heights)
-
-        for i in range(len(heights)) :
-            while len(stack) > 0 and heights[i] < stack[-1].val : 
-                popped = stack.pop(-1)
-                result[popped.index] = i
-            
-            stack.append(Pair(heights[i] , i))
-
-        while(len(stack)) > 0 : 
-            popped = stack.pop(-1)
-            result[popped.index] = len(heights)
-        
-        return result
-
-    def nextSmallerElementOnLeft(self, heights : [int]) -> [int] : 
-        stack = []
-        result = [-1]*len(heights)
-
-        for i in range(len(heights)-1 , -1, -1) : 
-            while len(stack) > 0 and heights[i] < stack[-1].val : 
-                popped = stack.pop(-1)
-                result[popped.index] = i
-
-            stack.append(Pair(heights[i] , i))
-        
-        while(len(stack)) > 0 :
-            popped = stack.pop(-1)
-            result[popped.index] = -1
-
-        return result
-
-
-class Pair : 
-    def __init__(self , value , index) : 
-        self.val = value
-        self.index = index
