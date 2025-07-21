cf6fd1b24da95a91e2f9be767aad6835717bbd16
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-21 14:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,48 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
         low = 0
-        high = len(nums) - 1
-        result = -1
+        high = len(nums)-1
 
-        while low <= high : 
-            
-            mid = low + ((high - low) // 2)
+        while low<=high : 
 
-            if nums[mid] > nums[high] : #we are in the left side of the array
-                
-                if target >= nums[low] and target <= nums[mid] : 
-                    result = self.doBinarySearch(low , mid , nums , target)
-                    return result
+            mid = low + (high - low) // 2
+
+            if nums[mid] == target : 
+                return mid
+
+            elif nums[high] <= nums[mid] : 
+                if target <= nums[mid] and target >= nums[low] : 
+                    high = mid - 1
                 
                 else : 
                     low = mid + 1
             
-            else : 
-
+            elif nums[high] > nums[mid] : 
                 if target >= nums[mid] and target <= nums[high] : 
-                    result = self.doBinarySearch(mid , high , nums , target)
-                    return result
+                    low = mid + 1
                 
                 else : 
                     high = mid - 1
             
-        
-        return result
-    
-    def doBinarySearch(self , low : int , high : int , nums : [int] , target : int) -> int : 
-
-        while low<=high : 
-
-            mid = low + ((high - low) // 2)
-
-            if nums[mid] == target : 
-                return mid
-            
-            elif nums[mid] < target : 
-                low = mid + 1
-            
-            else :
-                high = mid - 1
         
         return -1
