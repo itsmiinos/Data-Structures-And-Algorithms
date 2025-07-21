6c9b4a0a76900f4cbd82b7854ad33c2baca39cd8
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-21 21:31:47 +0530

    Time: 164 ms (12.12%) | Memory: 22.5 MB (69.91%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,33 @@
+import math
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        max_divisor = -float('inf')
+
+        for i in range(len(nums)) : 
+            max_divisor = max(max_divisor , nums[i])
+        
+        result = self.doBinarySearch(1 , max_divisor , nums , threshold) 
+        return result
+    
+    def doBinarySearch(self , low : int , high : int , nums : list[int] , threshold : int) -> int : 
+
+        ans = 0 
+        while low <= high : 
+            mid = low + (high - low) // 2
+
+            if self.calculateSum(mid , nums) <= threshold : 
+                ans = mid
+                high = mid - 1
+            
+            else : 
+                low = mid + 1
+        
+        return ans
+
+    def calculateSum(self , n : int , nums : list[int]) -> int : 
+
+        sum = 0
+        for i in range(len(nums)) : 
+            sum += math.ceil(nums[i] / n)
+        
+        return sum
