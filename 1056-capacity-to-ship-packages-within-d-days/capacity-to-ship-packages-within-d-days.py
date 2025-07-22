1ea546f8e4390f84cafd38d3bb990ec592c95bfd
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-22 21:05:45 +0530

    Time: 283 ms (12.68%) | Memory: 22 MB (97.94%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,51 +1,38 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-
-        max_weight = -float('inf')
-        sum_of_all_weights = 0
+        min_cap = -float('inf')
+        max_cap = 0
 
         for i in range(len(weights)) : 
-            max_weight = max(max_weight , weights[i])
-            sum_of_all_weights += weights[i]
-        
-        # we got the high = sum_of_weights which will require 1 day and we got the low = max_weight which will require approx lenght of array if all the array is equal in the worst case
+            min_cap = max(min_cap , weights[i])
+            max_cap += weights[i]
 
-        result = self.doBinarySearch(max_weight , sum_of_all_weights , days , weights)
+        result = self.doBinarySearchOnAnswer(min_cap , max_cap , weights , days) 
         return result
-    
-    def doBinarySearch(self , low : int , high : int , days : int , weights : [int]) -> int : 
+
+    def doBinarySearchOnAnswer(self , low : int , high : int , weights : list[int] , days : int) -> int : 
         ans = 0
+
         while low<=high : 
-            
-            mid = low + ((high - low) // 2)
-            
-            if self.checkDays(mid ,weights) <= days : 
+            mid = low + (high - low)//2 
+
+            if self.calculateDays(mid , weights) <= days :
                 ans = mid
                 high = mid - 1
-            
+
             else : 
                 low = mid + 1
-        
+
         return ans
-    
-    def checkDays(self , w : int , weights : [int]) -> int : 
-        
-        i = 0 
-        running_sum = 0
+
+    def calculateDays(self , n : int , weights : list[int]) -> int : 
         count = 0
-        while i < len(weights) : 
-            
-            if running_sum + weights[i] > w : 
-                count+=1
+        running_sum = 0
+
+        for i in range(len(weights)) :
+            if running_sum + weights[i] > n : 
                 running_sum = 0
-            
+                count += 1
             running_sum += weights[i]
-            i+=1
-
-        return count + 1
-            
-
         
-        
-
-        
+        return count+1
