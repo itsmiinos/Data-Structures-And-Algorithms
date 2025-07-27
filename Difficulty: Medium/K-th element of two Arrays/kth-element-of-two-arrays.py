925ac46f50b4610a6f460d562643fa26fbc42c97
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-07-27 20:45:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -7,26 +7,29 @@ class Solution:
         n1 = len(a)
         n2 = len(b)
         
-        if n1 < n2 : 
+        if n1 > n2 : 
             return self.kthElement(b , a , k)
-        
-        low = max(k-n2 , 0)
-        high = min(k , n1)
+            
         left = k
+        low = max(0 , k - n2)
+        high = min(k , n1)
         
         while low <= high : 
             mid1 = low + (high - low) // 2
             mid2 = left - mid1
             
-            l1 = a[mid1-1] if mid1 - 1 >= 0  else  float('-inf')
-            l2 = b[mid2 - 1] if mid2 - 1 >= 0  else  float('-inf')
+            l1 = a[mid1-1] if mid1 - 1 >= 0 else float('-inf')
+            l2 = b[mid2-1] if mid2 - 1 >= 0 else float('-inf')
             r1 = a[mid1] if mid1 < n1 else float('inf')
             r2 = b[mid2] if mid2 < n2 else float('inf')
             
             if l1 <= r2 and l2 <= r1 : 
                 return max(l1 , l2)
             
-            elif l1 > r2 : high = mid1 - 1
-            else : low = mid1 + 1
+            elif l1 > r2 : 
+                high = mid1 - 1
             
+            else : 
+                low = mid1 + 1
+        
         return 0
