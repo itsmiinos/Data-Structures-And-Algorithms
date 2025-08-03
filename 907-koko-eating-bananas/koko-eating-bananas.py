63cb9629cf9e9a2119d5dcae5d5e6d50212ecec7
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-03 19:45:45 +0530

    Time: 193 ms (18.06%) | Memory: 18.9 MB (47.45%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,32 +1,35 @@
 import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        max_bananas = -float('inf')
+        low = 1 #as koko can eat atleast 1 banana per hr
+        high = 0 #sum of all the bananas in 1 hr
 
         for i in range(len(piles)) : 
-            max_bananas = max(max_bananas , piles[i])
-
-        result = self.doBinarySearch(1 , max_bananas , piles , h)
+            high = max(high , piles[i])
+        
+        result = self.doBinarySearch(low , high , piles , h)
         return result
     
-    def doBinarySearch(self , low : int , high : int , piles : list[int] , h : int) -> int : 
-        ans = 0
+    def doBinarySearch(self, low : int , high : int , piles : list[int] , h : int) -> int : 
+        ans = float('inf')
+
         while low <= high : 
+            mid = low + ((high - low) // 2)
 
-            mid = low + (high - low) // 2
-
-            if self.calculateHours(mid , piles) > h : 
-                low = mid + 1
-            
-            else : 
+            if self.calculateBananasEaten(mid , piles) <= h : 
                 ans = mid
                 high = mid - 1
+            
+            else : 
+                low = mid + 1
         
         return ans
     
-    def calculateHours(self , speed : int , piles : list[int]) -> int : 
-        time = 0
+    def calculateBananasEaten(self , speed : int , piles : list[int]) -> int : 
+
+        hour = 0
         for i in range(len(piles)) : 
-            time += math.ceil(piles[i] / speed)
+            pile = piles[i]
+            hour += math.ceil(pile / speed)
         
-        return time
+        return hour
