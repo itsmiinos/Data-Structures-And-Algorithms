4e87563f16bb8a72060db47080f42a2ac1611f45
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-03 20:12:57 +0530

    Time: 480 ms (16.01%) | Memory: 31.7 MB (8.31%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,43 +1,46 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        low = float('inf')
+        high = float('-inf')
 
-        if len(bloomDay) < m*k : # case where all the flowers cant make m * k
+        if len(bloomDay) < m*k : 
             return -1
-        
-        min_bloom_day = float('inf') # where we get only the first bloomed flower
-        max_bloom_day = -float('inf') # where we get all the flowers
 
         for i in range(len(bloomDay)) : 
-            min_bloom_day = min(min_bloom_day , bloomDay[i])
-            max_bloom_day = max(max_bloom_day , bloomDay[i])
-
-        result = self.doBinarySearch(min_bloom_day , max_bloom_day , m , k , bloomDay)
+            low = min(low , bloomDay[i])
+            high = max(high , bloomDay[i])
+        
+        result = self.doBinarySearch(low , high , bloomDay , m , k)
         return result
     
-    def doBinarySearch(self , low : int , high : int , m : int, k : int, bloomDay : list[int]) -> int : 
+    def doBinarySearch(self , low : int , high : int , bloomDay : list[int], m : int, k : int) -> int : 
         ans = 0
 
         while low <= high : 
-            mid = low + (high - low)//2
 
-            if self.calculateNumberOfFlowers(mid ,bloomDay , k) >= m : #we need to check whether the flowers in that day sums up to m*K
-                ans = mid # that number of days can make M*K flowers but we need the minnimum
+            mid = low + ((high - low) // 2)
+
+            if self.calculateNumberOfFlowers(mid , bloomDay , k) >= m : 
+                ans = mid
                 high = mid - 1
+
             else : 
                 low = mid + 1
         
         return ans
     
-    def calculateNumberOfFlowers(self , day : int, bloomDay : list[int] , k : int) -> int : 
-        running_sum = 0 
-        count = 0
-        for i in range(len(bloomDay)) : 
-            if bloomDay[i] <= day : 
-                running_sum+=1
-                if running_sum == k : 
-                    count+=1
-                    running_sum = 0
-            else : 
-                running_sum = 0
+    def calculateNumberOfFlowers(self , day : int, bloomDay : list[int], k : int) -> int : 
         
-        return count
+        count = 0
+        b_count = 0
+
+        for i in range(len(bloomDay)) : 
+            if day >= bloomDay[i] : 
+                count+=1
+                if count == k : 
+                    count = 0
+                    b_count +=1
+            else : 
+                count = 0
+
+        return b_count
