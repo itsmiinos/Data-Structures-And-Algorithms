f87adcc497a749dcc2fe5db071e08e44bdcdd27d
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-10 15:07:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.py
+++ Difficulty: Medium/Stock span problem/stock-span-problem.py
@@ -1,26 +1,17 @@
 class Solution:
     def calculateSpan(self, arr):
-        #write code here
+        # code here
         stack = []
-        ans = [0]*len(arr)
+        result = [1]*len(arr)
         
         for i in range(len(arr)) : 
-            while len(stack) > 0 and arr[i] >= stack[-1].val :
-                stack.pop(-1)
+            span_value = 1
+            while len(stack) > 0 and stack[-1][0] <= arr[i] : 
+                popped = stack.pop(-1)
+                index = popped[1]
+                span_value += result[index]
             
-            if not stack : 
-                ans[i] = i+1
-            
-            else : 
-                ans[i] = i - stack[-1].index
-            
-            stack.append(Pair(arr[i], i))
-        return ans
-            
+            stack.append([arr[i] , i])
+            result[i] = span_value
         
-
-class Pair :
-    
-    def __init__ (self , value : int , index : int) :
-        self.val = value
-        self.index = index
+        return result
