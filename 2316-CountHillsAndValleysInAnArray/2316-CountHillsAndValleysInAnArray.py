1fdb5bc1b9489bbab2ed674e498993ecf7f2488c
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:14:06 +0530

    Create 2316-CountHillsAndValleysInAnArray

---
+++ 2316-CountHillsAndValleysInAnArray/2316-CountHillsAndValleysInAnArray.py
@@ -0,0 +1,16 @@
+# Last updated: 8/13/2025, 8:16:10 PM
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        count = 0
+        prev = nums[0]
+
+        for i in range(1 , len(nums)-1) : 
+            if nums[i] == nums[i+1] : 
+                continue
+            
+            if (nums[i] < prev and nums[i] < nums[i+1] ) or (nums[i] > prev  and nums[i] > nums[i+1]) : 
+                count+=1
+            
+            prev = nums[i]
+        
+        return count
