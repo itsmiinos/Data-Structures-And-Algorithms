c290290ef430da91f8bc54299a1b37108d3111e5
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:15:18 +0530

    Create 1510-FindLuckyIntegerInAnArray

---
+++ 1510-FindLuckyIntegerInAnArray/1510-FindLuckyIntegerInAnArray.py
@@ -0,0 +1,15 @@
+# Last updated: 8/13/2025, 8:17:22 PM
+import sys
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        my_map = {}
+
+        for i in range(len(arr)) : 
+            my_map[arr[i]] = my_map.get(arr[i] , 0) + 1
+
+        maxi = -sys.maxsize
+        for key in my_map : 
+            if my_map[key] == key : 
+                maxi = max(maxi , key)
+
+        return maxi if maxi >=1 else -1
