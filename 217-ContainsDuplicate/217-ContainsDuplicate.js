69dffd558f017b37a34379478f6bc8959f099346
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:19:09 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -0,0 +1,17 @@
+// Last updated: 8/13/2025, 8:21:13 PM
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let mySet = new Set();
+    for(let i = 0; i<nums.length ; i++){
+        if(!mySet.has(nums[i])){
+            mySet.add(nums[i]);
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+};
