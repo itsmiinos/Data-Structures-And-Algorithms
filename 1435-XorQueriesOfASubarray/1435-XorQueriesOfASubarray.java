f20e8e503036d0566176f89e613e56f789fd26bb
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:15:24 +0530

    Create 1435-XorQueriesOfASubarray

---
+++ 1435-XorQueriesOfASubarray/1435-XorQueriesOfASubarray.java
@@ -0,0 +1,22 @@
+// Last updated: 8/13/2025, 8:17:28 PM
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int n=arr.length;
+        int[] pre=new int[n];
+        pre[0]=arr[0];
+        for(int i=1;i<n;i++) {
+            pre[i]=pre[i-1]^arr[i];
+        }
+        int[] res=new int[queries.length];
+        for(int k=0;k<queries.length;k++) {
+            int i=queries[k][0];
+            int j=queries[k][1];
+            if(i==0) {
+                res[k]=pre[j];
+            } else {
+                res[k]=pre[j]^pre[i-1];
+            }
+        }
+        return res;
+    }
+}
