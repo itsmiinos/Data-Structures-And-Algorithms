16a60464ff451ae11e600031ca9c61ad1c11bb7e
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:19:25 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.py
@@ -0,0 +1,19 @@
+# Last updated: 8/13/2025, 8:21:30 PM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1]*(len(nums)+1)
+        result = self.helper(len(nums)-1 , nums , dp)
+        return result
+
+    def helper(self , i:int , nums : [int] , dp : [int]) -> int : 
+        if i < 0 : 
+            return 0
+        if i ==0:
+            return nums[i]
+        if dp[i] !=-1 : return dp[i]
+        temp1 = self.helper(i-2 , nums , dp) + nums[i]
+        temp2 = self.helper(i-1 , nums , dp)
+
+        ans = max(temp1 , temp2)
+        dp[i] = ans
+        return ans
