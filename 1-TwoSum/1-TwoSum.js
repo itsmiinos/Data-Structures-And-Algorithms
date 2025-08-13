5de2055e1d3e0887beaf5374aa37bb1ebe734152
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:22:44 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,18 @@
+// Last updated: 8/13/2025, 8:24:49 PM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let sumMap = new Map();
+    for(let i=0;i<nums.length;i++){
+        const diff = target - nums[i];
+        if(sumMap.has(diff)){
+            return [i,sumMap.get(diff)];
+        }
+        else{
+            sumMap.set(nums[i],i);
+        }
+    }
+};
