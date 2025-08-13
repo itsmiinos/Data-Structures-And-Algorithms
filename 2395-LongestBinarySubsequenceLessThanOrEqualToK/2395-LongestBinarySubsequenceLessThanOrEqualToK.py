9374622fdd9144261ee1f75b4dfb8bfddadbd67c
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:14:03 +0530

    Create 2395-LongestBinarySubsequenceLessThanOrEqualToK

---
+++ 2395-LongestBinarySubsequenceLessThanOrEqualToK/2395-LongestBinarySubsequenceLessThanOrEqualToK.py
@@ -0,0 +1,19 @@
+# Last updated: 8/13/2025, 8:16:07 PM
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        length = 0
+        power = 1
+        for i in reversed(s) : 
+            if i == '0' : 
+                length +=1
+                power = power * 2
+            else : 
+                if power <= k and int(i) * power <= k : 
+                    k = k - int(i) * power
+                    length+=1
+                    power = power * 2
+                
+        
+        return length
+
+              
