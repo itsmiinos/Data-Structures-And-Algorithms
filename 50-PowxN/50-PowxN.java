97ebe826c454fc2a49c962fe316cd3db92f80640
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:41 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,22 @@
+// Last updated: 8/13/2025, 8:23:46 PM
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0) return myUtils(1/x,Math.abs((long)n));
+        else return myUtils(x,n);
+    }
+
+    public double myUtils(double x , long n){
+        if(n==0){
+            return 1.0;
+        }
+        if(x==0){
+            return 0.0;
+        }
+        if(n%2==0){
+            return myUtils(x*x,n/2);
+        }
+        else{
+            return x * myUtils(x,n-1);
+        }
+    }
+}
