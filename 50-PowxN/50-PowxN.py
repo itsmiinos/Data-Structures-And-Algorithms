d5b88f0d9c7ed52ff853ab541edf8499b2c774ca
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:37 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,15 @@
+# Last updated: 8/13/2025, 8:23:41 PM
+class Solution(object):
+    def myPow(self, x, n):
+        if n < 0:
+            n = -n
+            x = 1/x
+        if n ==0 :
+            return 1
+        if n % 2 == 0:
+            half = self.myPow(x, n // 2)
+            return half * half
+        else:
+            half = self.myPow(x, (n - 1) // 2)
+            return x * half * half
+        
