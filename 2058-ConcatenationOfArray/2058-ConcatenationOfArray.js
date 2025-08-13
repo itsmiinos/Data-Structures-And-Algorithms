9a5ab4e059bbfbccea4070b06bd9ebf3a1671040
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:14:40 +0530

    Create 2058-ConcatenationOfArray

---
+++ 2058-ConcatenationOfArray/2058-ConcatenationOfArray.js
@@ -0,0 +1,18 @@
+// Last updated: 8/13/2025, 8:16:45 PM
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let ans = new Array(nums.length*2);
+    let k = 0;
+    for(let i=0;i<nums.length;i++){
+        ans[k] = nums[i];
+        k++;
+    }
+    for(let i=0;i<nums.length;i++){
+        ans[k] = nums[i];
+        k++;
+    }
+    return ans;
+};
