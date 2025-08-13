ceec387bab1ec7a139f4973810d66554d455d75c
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:16:43 +0530

    Create 812-RotateString

---
+++ 812-RotateString/812-RotateString.java
@@ -0,0 +1,9 @@
+// Last updated: 8/13/2025, 8:18:47 PM
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
