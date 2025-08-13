fed5c1f431fe9c2b422d3f8556cc2c67d08b7cf1
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 09:58:10 +0530

    Time: 1596 ms (35.87%) | Memory: 18.6 MB (58.47%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,25 @@
+M = int(1e9 + 7)
+
+class Solution:
+    def __init__(self):
+        self.memo = [[-1] * 301 for _ in range(301)]
+
+    def func(self, n, sum_, x, num):
+        if sum_ == n:
+            return 1
+        tmp = pow(num, x)
+
+        if sum_ + tmp > n:
+            return 0
+
+        if self.memo[num][sum_] != -1:  # added
+            return self.memo[num][sum_]
+
+        take = self.func(n, sum_ + tmp, x, num + 1)
+        not_take = self.func(n, sum_, x, num + 1)
+
+        self.memo[num][sum_] = (take + not_take) % M  # updated
+        return self.memo[num][sum_]
+
+    def numberOfWays(self, n, x):
+        return self.func(n, 0, x, 1)
