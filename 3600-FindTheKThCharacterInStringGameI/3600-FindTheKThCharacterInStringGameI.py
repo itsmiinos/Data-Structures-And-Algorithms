b9adf28e43e7868c4023a4e46164eef2c6628c63
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:13:02 +0530

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.py
@@ -0,0 +1,26 @@
+# Last updated: 8/13/2025, 8:15:07 PM
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        word = "a"
+        result = ""
+        while len(result) < k  : 
+            result = self.createNewString(word)
+            word = result
+        return result[k-1]
+    
+    def createNewString(self , word : str) -> str :
+        result = "" 
+        for i in range(len(word)) : 
+            if word[i] == 'z' : 
+                result+= 'a'
+            else :
+                result += chr((ord(word[i]) + 1))
+            # print(word)
+        
+        return word+result
+
+
+        
+        
+        
+        
