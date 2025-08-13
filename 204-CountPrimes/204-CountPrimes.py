7be9822d9a682888ad356d06c50b2aeec1de574d
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:19:21 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.py
@@ -0,0 +1,20 @@
+# Last updated: 8/13/2025, 8:21:25 PM
+import math
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        count = 0 
+        if n<=2 : 
+            return 0
+        prime = [True]*(n)
+        prime[0] = False
+        prime[1] = False
+        for i in range(2 , int(math.sqrt(n))+1) : 
+            if prime[i] == True : 
+                for j in range (i*i , n , i) : 
+                    prime[j] = False
+        
+        for i in range(n) : 
+            if prime[i] : 
+                count+=1
+        
+        return count
