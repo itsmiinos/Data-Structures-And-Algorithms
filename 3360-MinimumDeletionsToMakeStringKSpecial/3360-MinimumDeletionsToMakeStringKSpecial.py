d69168cc9536d9e30f1ca8d96c163806767bfe69
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:13:23 +0530

    Create 3360-MinimumDeletionsToMakeStringKSpecial

---
+++ 3360-MinimumDeletionsToMakeStringKSpecial/3360-MinimumDeletionsToMakeStringKSpecial.py
@@ -0,0 +1,23 @@
+# Last updated: 8/13/2025, 8:15:27 PM
+import sys
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        feq_map = {}
+
+        for i in range(len(word)) : 
+            feq_map[word[i]] = feq_map.get(word[i] , 0) + 1
+        
+        min_count_del = sys.maxsize 
+
+        for val1 in feq_map : 
+            count_del = 0
+            for val2 in feq_map : 
+                if val1 != val2 : 
+                    if feq_map[val2] < feq_map[val1] : 
+                        count_del += feq_map[val2]
+                    elif feq_map[val2] > feq_map[val1] + k : 
+                        count_del += feq_map[val2] - (feq_map[val1] + k)
+
+            min_count_del = min(min_count_del , count_del)
+
+        return min_count_del
