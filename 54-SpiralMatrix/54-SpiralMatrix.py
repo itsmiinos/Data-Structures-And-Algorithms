1366b257f48316caec1e8fa4998cf6531c05ef3b
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:32 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/54-SpiralMatrix.py
@@ -0,0 +1,49 @@
+# Last updated: 8/13/2025, 8:23:37 PM
+class Solution:
+    def spiralOrder(self, matrix):
+        ans = []
+        row = len(matrix)
+        col = len(matrix[0])
+
+        i = 0
+        j = 0
+        rsteps = row - 1
+        csteps = col - 1
+        count = 0
+
+        while rsteps >= 1 and csteps >= 1:
+            for k in range(csteps):
+                ans.append(matrix[i][j])
+                j += 1
+                count += 1
+
+            for k in range(rsteps):
+                ans.append(matrix[i][j])
+                i += 1
+                count += 1
+
+            for k in range(csteps, 0, -1):
+                ans.append(matrix[i][j])
+                j -= 1
+                count += 1
+
+            for k in range(rsteps, 0, -1):
+                ans.append(matrix[i][j])
+                i -= 1
+                count += 1
+
+            rsteps -= 2
+            csteps -= 2
+            i += 1
+            j += 1
+
+        if rsteps == 0:
+            for k in range(csteps + 1):
+                ans.append(matrix[i][j])
+                j += 1
+        elif csteps == 0:
+            for k in range(rsteps + 1):
+                ans.append(matrix[i][j])
+                i += 1
+
+        return ans
