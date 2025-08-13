25d6a6d2ce182765fd0153b9232e31eb7d825d4a
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:44 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -0,0 +1,20 @@
+# Last updated: 8/13/2025, 8:23:49 PM
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        runningMax = [None]*n
+        backwardMax = [None]*n
+
+        runningMax[0] = height[0]
+        for i in range(1 , n) : 
+            runningMax[i] = max(runningMax[i-1] , height[i])
+        
+        backwardMax[n-1] = height[n-1]
+        for i in range(n-2 , -1 , -1):
+            backwardMax[i] = max(backwardMax[i+1] , height[i])
+        
+        total_collection = 0
+        for i in range(n) : 
+            total_collection += min(runningMax[i] , backwardMax[i]) - height[i]
+        
+        return total_collection
