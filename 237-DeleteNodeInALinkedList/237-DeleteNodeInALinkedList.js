5c9a9f397a40dd8adb87a64de7d690faacaef616
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:18:46 +0530

    Create 237-DeleteNodeInALinkedList

---
+++ 237-DeleteNodeInALinkedList/237-DeleteNodeInALinkedList.js
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2025, 8:20:51 PM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
