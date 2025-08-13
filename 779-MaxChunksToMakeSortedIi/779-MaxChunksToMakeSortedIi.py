67433efb04fcc971c6755bdde7b3b40014689b91
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:16:56 +0530

    Create 779-MaxChunksToMakeSortedIi

---
+++ 779-MaxChunksToMakeSortedIi/779-MaxChunksToMakeSortedIi.py
@@ -0,0 +1,23 @@
+# Last updated: 8/13/2025, 8:19:01 PM
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+
+        prefixMax = [None]*n
+        suffixMin = [None]*n
+
+        prefixMax[0] = arr[0]
+        for i in range(1 , n) : 
+            prefixMax[i] = max(prefixMax[i-1] , arr[i])
+        
+        suffixMin[n-1] = arr[n-1]
+        for i in range(n-2 , -1 , -1) : 
+            suffixMin[i] = min(suffixMin[i+1] , arr[i])
+
+        count = 0 
+
+        for i in range(n-1) : 
+            if prefixMax[i] <= suffixMin[i+1] : 
+                count+=1
+        
+        return count+1
