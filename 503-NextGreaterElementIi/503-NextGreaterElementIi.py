760c31e5f0557a176be162349e8966caee440804
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:17:28 +0530

    Create 503-NextGreaterElementIi

---
+++ 503-NextGreaterElementIi/503-NextGreaterElementIi.py
@@ -0,0 +1,17 @@
+# Last updated: 8/13/2025, 8:19:32 PM
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        result = [-1]*len(nums)
+        stack = []
+
+        i = 0 
+        while i < 2 * len(nums) : 
+            curr = nums[i%len(nums)]
+            while len(stack) > 0 and nums[stack[-1]] < curr : 
+                index = stack.pop(-1)
+                result[index] = curr
+            if i < len(nums) : 
+                stack.append(i)
+            i+=1
+
+        return result
