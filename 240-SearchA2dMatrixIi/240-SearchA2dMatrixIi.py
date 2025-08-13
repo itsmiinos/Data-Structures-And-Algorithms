fc683cb7490851831bb8ea1e17d29d4a1bcad7df
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:18:30 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.py
@@ -0,0 +1,21 @@
+# Last updated: 8/13/2025, 8:20:35 PM
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+
+
+        row = 0 
+        col = m-1
+
+        while row < n and col >= 0  : 
+            if matrix[row][col] == target : 
+                return True
+            
+            elif matrix[row][col] < target : 
+                row +=1
+            
+            else : 
+                col -=1
+        
+        return False
