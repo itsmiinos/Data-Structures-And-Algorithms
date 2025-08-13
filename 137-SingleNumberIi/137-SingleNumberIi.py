113d16241f88f57cf1acc0d8a596cf2ac48dc308
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:20:18 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.py
@@ -0,0 +1,22 @@
+# Last updated: 8/13/2025, 8:22:22 PM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(32) : 
+            count = 0
+            for j in range(len(nums)) : 
+                if self.checkBit(nums[j] , i) : 
+                    count +=1
+                
+            if count%3 != 0 : 
+                ans = ans + (1<<i) 
+
+        if ans >= 2**31:
+            ans -= 2**32
+
+        return ans
+
+    def checkBit(self , n : int , i : int) -> bool : 
+        if n & (1<<i) : 
+            return True
+        return False
