ad125e5d967d9b11c6b1cc7647220d8699ac4311
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:28 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.py
@@ -0,0 +1,19 @@
+# Last updated: 8/13/2025, 8:23:32 PM
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for j in range(n)] for i in range(m)]
+        result = self.helper(m-1 , n-1 , dp)
+        return result
+
+    def helper(self , i : int , j : int , dp : list[list[int]]) : 
+        if i<0 or j<0 : 
+            return 0
+        if i == 0 and j == 0 : 
+            return 1
+        if dp[i][j] !=-1 : return dp[i][j]
+
+        temp1 = self.helper(i-1 , j , dp)
+        temp2 = self.helper(i , j-1 , dp) 
+
+        dp[i][j] = temp1 + temp2
+        return temp1 + temp2
