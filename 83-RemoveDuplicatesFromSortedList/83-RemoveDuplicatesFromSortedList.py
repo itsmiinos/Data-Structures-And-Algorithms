16d7b38e50d6abf22f838aa6528d981ce6a21c15
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:00 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,19 @@
+# Last updated: 8/13/2025, 8:23:05 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    
+        temp = head
+
+        while temp is not None and temp.next is not None : 
+            if temp.next.val == temp.val : 
+                temp.next = temp.next.next
+            else : 
+                temp = temp.next
+        
+        
+        return head
