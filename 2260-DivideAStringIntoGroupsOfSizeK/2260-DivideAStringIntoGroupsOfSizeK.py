b2d54c1e65de1075e5c4c2c311b763a3f2d2bc80
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:14:10 +0530

    Create 2260-DivideAStringIntoGroupsOfSizeK

---
+++ 2260-DivideAStringIntoGroupsOfSizeK/2260-DivideAStringIntoGroupsOfSizeK.py
@@ -0,0 +1,23 @@
+# Last updated: 8/13/2025, 8:16:15 PM
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+
+        while len(s)%k != 0 : 
+            s = s + fill
+
+        l = len(s)
+        
+        result = []
+        last_added_index = 0
+        count = 0
+
+        for i in range(l) : 
+            if count==k: 
+                result.append(s[last_added_index : i])
+                last_added_index = i
+                count=0
+            count+=1
+        result.append(s[last_added_index : l])
+        
+        return result
+        
