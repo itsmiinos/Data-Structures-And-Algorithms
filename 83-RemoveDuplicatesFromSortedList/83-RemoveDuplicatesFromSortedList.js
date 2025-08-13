1a634c3a0c96995746b70196f7501b2fcda6b562
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:21:07 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,32 @@
+// Last updated: 8/13/2025, 8:23:11 PM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+class ListNode{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+var deleteDuplicates = function(head) {
+    let temp = head;
+
+    while(temp!=null && temp.next!=null){
+        if(temp.val === temp.next.val){
+            temp.next = temp.next.next;
+        }else{
+        temp = temp.next;}
+    }
+
+    return head;
+};
