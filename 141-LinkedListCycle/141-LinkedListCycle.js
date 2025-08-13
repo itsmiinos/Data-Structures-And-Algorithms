e5bf2a443673396102f4a769fdadacaa7e6d0f17
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:20:16 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.js
@@ -0,0 +1,26 @@
+// Last updated: 8/13/2025, 8:22:21 PM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast !==null && fast.next!==null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow===fast){
+            return true;
+        }
+    }
+    return false;
+};
