e1f7d7e4b5ae6a622051903dfa6d2c87a55f6caf
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-08-13 20:14:07 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.py
@@ -0,0 +1,33 @@
+# Last updated: 8/13/2025, 8:16:12 PM
+class Solution(object):
+    def rearrangeArray(self, nums):
+        negativeStack = []
+        positiveStack = []
+        ans = []
+
+        for i in range(len(nums)) :
+            if nums[i] < 0 :
+                negativeStack.append(nums[i])
+            else :
+                positiveStack.append(nums[i])
+        
+        print(negativeStack)
+        print(positiveStack)
+        
+        pi = 0
+        ni = 0
+        index = 0
+        previous = -1
+
+        while index<len(nums) :
+            if previous < 0 :
+                ans.append(positiveStack[pi])
+                pi+=1
+            else : 
+                ans.append(negativeStack[ni])
+                ni+=1
+            previous = ans[index]
+            index+=1
+        
+        return ans
+        
