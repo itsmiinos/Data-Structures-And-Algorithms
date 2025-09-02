870af9f699537731f7d69258a0d8de6e53ec43b5
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-09-02 22:59:34 +0530

    Create 322-CoinChange

---
+++ 322-CoinChange/version/python/322-CoinChange_20250902_230155.py
@@ -0,0 +1,33 @@
+# Last updated: 9/2/2025, 11:01:55 PM
+class Solution:
+    def coinChange(self, coins: List[int], sum: int) -> int:
+        dp = [[-1 for i in range(sum+1)] for i in range(len(coins))]
+        result = self.doDFS(coins , len(coins)-1 , sum , dp)
+        if result != float('inf'):
+            return result 
+        else :
+            return -1
+        
+    
+    def doDFS(self , coins : list[int] , n : int , sum : int , dp : list[list[int]]) -> int : 
+        if sum == 0 : 
+            return 0
+        
+        if sum!=0 and n < 0 : 
+            return float('inf')
+        
+        if dp[n][sum]!= -1 : 
+            return dp[n][sum]
+        
+        if coins[n] <= sum : 
+            taken = self.doDFS(coins , n , sum - coins[n] , dp) + 1
+            not_taken = self.doDFS(coins , n-1 , sum  , dp)
+            
+            dp[n][sum] = min(taken , not_taken)
+            
+            return dp[n][sum]
+        
+        else : 
+            
+            dp[n][sum] = self.doDFS(coins , n-1 , sum , dp)
+            return dp[n][sum]
