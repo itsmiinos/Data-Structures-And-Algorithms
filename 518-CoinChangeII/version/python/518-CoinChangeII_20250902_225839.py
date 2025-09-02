6001914f670d1259a390aedf87a4bfb63cd0ddd1
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-09-02 22:56:18 +0530

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/version/python/518-CoinChangeII_20250902_225839.py
@@ -0,0 +1,31 @@
+# Last updated: 9/2/2025, 10:58:39 PM
+class Solution:
+    def change(self, sum: int, coins: List[int]) -> int:
+              
+        dp = [[-1 for i in range(sum+1)] for i in range(len(coins))]
+        result = self.doDFS(coins , len(coins)-1 , sum , dp)
+        return result
+        
+    
+    def doDFS(self , coins : list[int] , n : int , sum : int , dp : list[list[int]]) -> int : 
+        if sum == 0 : 
+            return 1
+        
+        if sum!=0 and n < 0 : 
+            return 0
+        
+        if dp[n][sum]!= -1 : 
+            return dp[n][sum]
+        
+        if coins[n] <= sum : 
+            taken = self.doDFS(coins , n , sum - coins[n] , dp)
+            not_taken = self.doDFS(coins , n-1 , sum  , dp)
+            
+            dp[n][sum] = taken + not_taken
+            
+            return dp[n][sum]
+        
+        else : 
+            
+            dp[n][sum] = self.doDFS(coins , n-1 , sum , dp)
+            return dp[n][sum]
