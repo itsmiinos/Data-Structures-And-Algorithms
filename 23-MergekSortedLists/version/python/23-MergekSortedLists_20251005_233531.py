9cbfb1d84fde92160ceb93e030e99f0a78fae0ff
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-10-05 23:32:42 +0530

    Create 23-MergekSortedLists

---
+++ 23-MergekSortedLists/version/python/23-MergekSortedLists_20251005_233531.py
@@ -0,0 +1,26 @@
+# Last updated: 10/5/2025, 11:35:31 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        for l in lists :
+            if l is not None : 
+                heapq.heappush(heap, (l.val, id(l), l))
+            
+
+        dummy = ListNode(0)
+        current = dummy
+        while heap:
+            _, _, node = heapq.heappop(heap)
+            current.next = node
+            current = current.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, id(node.next), node.next))
+        
+
+        return dummy.next
