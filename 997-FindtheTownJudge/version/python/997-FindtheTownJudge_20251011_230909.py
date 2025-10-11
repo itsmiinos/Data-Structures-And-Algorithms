51bd4bc742223a56d5098da970f5aa6762b65627
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-10-11 23:06:14 +0530

    Create 997-FindtheTownJudge

---
+++ 997-FindtheTownJudge/version/python/997-FindtheTownJudge_20251011_230909.py
@@ -0,0 +1,20 @@
+# Last updated: 10/11/2025, 11:09:09 PM
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree = [0]*(n+1)
+        outdegree = [0]*(n+1)
+
+        if not trust and n == 1 : 
+            return 1
+    
+        for u , v in trust : 
+            indegree[v] +=1
+            outdegree[u] +=1
+        
+        for i in range(n+1) : 
+            if indegree[i] == n-1 and outdegree[i] == 0 :
+                return i
+        
+        return -1
+        
+        
