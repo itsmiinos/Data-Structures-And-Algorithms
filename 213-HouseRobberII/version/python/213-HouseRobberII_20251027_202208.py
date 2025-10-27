5da4c33d1528498d0f2c5c5991d6d648f2d19f94
Yudhajit <yudhajitchowdhury17@gmail.com>
2025-10-27 20:19:00 +0530

    Create 213-HouseRobberII

---
+++ 213-HouseRobberII/version/python/213-HouseRobberII_20251027_202208.py
@@ -0,0 +1,27 @@
+# Last updated: 10/27/2025, 8:22:08 PM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp1 = [-1]*len(nums)
+        dp2 = [-1]*len(nums)
+        return max(
+            self.robHelper(nums, 0, len(nums) - 2 , dp1),  # Exclude last house
+            self.robHelper(nums, 1, len(nums) - 1 , dp2)   # Exclude first house
+        )
+    
+    def robHelper(self , nums , start , end , dp) -> int : 
+        if start > end:
+            return 0
+        if start == end:
+            return nums[start]
+        if dp[end] !=  -1 : return dp[end]
+        # Recursive relation
+        dp[end] = max(
+            self.robHelper(nums, start, end - 1 , dp),            
+            self.robHelper(nums, start, end - 2 , dp) + nums[end] 
+        )
+        return max(
+            self.robHelper(nums, start, end - 1 , dp),            
+            self.robHelper(nums, start, end - 2 , dp) + nums[end] 
+        )
