4b37a0022c12e260799d88bd4de83e1706ab7e54
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-02-05 13:41:24 +0530

    Create 3379-TransformedArray

---
+++ 3379-TransformedArray/version/python/3379-TransformedArray_20260205_134125.py
@@ -0,0 +1,25 @@
+# Last updated: 2/5/2026, 1:41:25 PM
+1class Solution:
+2    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+3        result = [0]*len(nums)
+4
+5        for i in range(len(nums)) :
+6            val = nums[i]
+7            if val > 0 :
+8                ind = i
+9                # while val > 0 :
+10                #     ind = (ind + 1) % len(nums)
+11                #     val-=1
+12                result[i] = nums[(i + val) % len(nums)]
+13            elif val < 0 :
+14                ind = i
+15                # val = abs(val)
+16                # while val > 0 :
+17                #     ind = ((ind - 1) + len(nums)) % len(nums)
+18                #     val-=1
+19                result[i] = nums[(i + val) % len(nums)]
+20            else :
+21                result[i] = nums[i]
+22        
+23        return result
+24            
