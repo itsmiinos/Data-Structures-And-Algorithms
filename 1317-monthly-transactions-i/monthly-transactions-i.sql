b517dd81a9b4775a64aceedfbad6db2078dc15f5
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-08-22 20:05:44 +0530

    Time: 574 ms (86.69%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -6,3 +6,15 @@ sum(amount) as trans_total_amount ,
 sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
 from transactions
 group by country , date_format(trans_date , '%Y-%m');
+
+# Pyspark : 
+-- result = (transactions
+--         .groupBy(date_format(col("trans_date") , 'yyyy-MM') , col("country"))
+--         .agg(
+--             count(col("id")).alias("trans_count"),
+--             sum(when(col("state") == "approved" , 1).otherwise(0)).alias("approved_count"),
+--             sum(col("amount")).alias("trans_total_amount"),
+--             sum(when(col("state") == "approved" , col("amount")).otherwise(0)).alias("approved_total_amount")
+--         )
+
+-- )
