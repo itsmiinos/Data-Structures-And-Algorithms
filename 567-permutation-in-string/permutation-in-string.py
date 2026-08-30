ffcef37e6d8b20fbc1819a225f0003fdca3d1477
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-08-30 18:35:31 +0530

    Time: 15 ms (80.74%) | Memory: 19.3 MB (86.52%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,30 +2,28 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2) :
             return False
-        
-        s1_map = collections.defaultdict(int)
+
+        s1_map = [0]*26
+        s2_map = [0]*26
+
         for i in range(len(s1)) :
-            s1_map[s1[i]] +=1
+            s1_map[ord(s1[i]) - ord('a')] +=1
         
-        i = 0
+        i = 0 
         j = 0
 
-        s2_map = collections.defaultdict(int)
+        while (j - i + 1) < len(s1) :
+            s2_map[ord(s2[j]) - ord('a')] +=1
+            j+=1
 
         while j < len(s2) :
-            while j - i + 1 < len(s1) :
-                s2_map[s2[j]] +=1
-                j+=1
 
-            s2_map[s2[j]] +=1
+            s2_map[ord(s2[j]) - ord('a')] +=1
 
-            if s2_map == s1_map :
+            if s1_map == s2_map :
                 return True
-
-            # print(s2_map , s1_map)
-            s2_map[s2[i]]-=1
-            if s2_map[s2[i]] == 0 :
-                del s2_map[s2[i]]
+            
+            s2_map[ord(s2[i]) - ord('a')] -=1
             i+=1
             j+=1
         
