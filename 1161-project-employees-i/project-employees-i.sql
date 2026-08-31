22dec90b0d4fac9fb9529ce2c25f95e63052d4d6
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-08-31 11:09:18 +0530

    Time: 583 ms (44.12%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,2 +1,15 @@
 # Write your MySQL query statement below
 select p.project_id , round(avg(e.experience_years), 2) as average_years from project as p inner join employee as e on p.employee_id = e.employee_id group by p.project_id;
+
+-- # Pyspark :
+-- result = (
+--         project.join(employee , project["employee_id"]==employee["employee_id"] , how="inner")
+--             .groupBy(project["project_id"])
+--             .agg(
+--                 round(avg(employee["experience_years"]),2).alias("average_years")
+--             )
+--             .select(
+--                 project["project_id"],
+--                 col("average_years")
+--             )
+-- )
