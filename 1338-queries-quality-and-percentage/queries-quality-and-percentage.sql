182e0c28a095524c877e7d1b835e3ac843ab4427
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-01 15:33:07 +0530

    Time: 369 ms (76.60%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,9 +1,17 @@
-SELECT
-    query_name,
-    ROUND(AVG(rating / position), 2) AS quality,
-    ROUND(
-        COUNT(CASE WHEN rating < 3 THEN 1 END) * 100.0 / COUNT(*),
-        2
-    ) AS poor_query_percentage
-FROM queries
-GROUP BY query_name;
+# Write your MySQL query statement below
+select query_name , round(avg(rating / position) , 2) as quality , 
+coalesce(round((sum(case when rating < 3 then 1 end) * 100.00 / count(query_name)) , 2), 0) as poor_query_percentage from queries group by query_name;
+
+# Pyspark :
+-- result = (queries.groupBy(col("query_name"))
+--             .agg(
+--                 round(avg(col("rating") / col("position")),2).alias("quality"),
+--                 coalesce(round(sum(when(col("rating") < 3 , 1)) * 100.00 / count("query_name"), 2) , 0).alias("poor_query_percentage")
+--             )
+--             .select(
+--                 col("query_name"),
+--                 col("quality"),
+--                 col("poor_query_percentage")
+--             )
+
+-- )
