78f950701cd345523924ff6c515c8573b3943cfb
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-01 21:25:15 +0530

    Time: 580 ms (5.04%) | Memory: 40.1 MB (5.71%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,46 +1,37 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        left_smallest_element = [-1]*len(heights)
-        right_smallest_element = [-1]*len(heights)
+        left_min_stack = [-1] * len(heights)
+        right_min_stack = [len(heights)] * len(heights)
 
-        left_stack = []
-        right_stack = []
+        my_stack = []
 
-        for i in range(len(heights) - 1 , -1 , -1) : 
-            while len(left_stack) > 0 and heights[i] < left_stack[-1][0] : 
-                popped = left_stack.pop(-1)
-                popped_value = popped[0]
-                popped_index = popped[1]
+        for i in range(len(heights)) :
+            while len(my_stack) > 0 and my_stack[-1][0] > heights[i] :
+                value , index = my_stack.pop(-1)
 
-                left_smallest_element[popped_index] = i
+                right_min_stack[index] = i
             
-            left_stack.append([heights[i] , i])
+            my_stack.append([heights[i] , i])
         
-        while len(left_stack) > 0 : 
-            popped = left_stack.pop(-1)
-            popped_index = popped[1]
+        my_stack = []
 
-            left_smallest_element[popped_index] = -1
-        
-        for i in range(len(heights)) : 
-            while len(right_stack) > 0 and heights[i] < right_stack[-1][0] : 
-                popped = right_stack.pop(-1)
-                popped_value = popped[0]
-                popped_index = popped[1]
+        for i in range(len(heights)-1 , -1 , -1) :
+            while len(my_stack) > 0 and my_stack[-1][0] > heights[i] :
+                value , index = my_stack.pop(-1)
 
-                right_smallest_element[popped_index] = i
+                left_min_stack[index] = i
             
-            right_stack.append([heights[i] , i])
+            my_stack.append([heights[i] , i])
         
-        while len(right_stack) > 0 : 
-            popped = right_stack.pop(-1)
-            popped_index = popped[1]
+        print(left_min_stack , right_min_stack)
+        max_area = float('-inf')
+        for i in range(len(heights)) :
+            max_distance_on_left = i - left_min_stack[i]
+            max_distance_on_right = right_min_stack[i] - i
 
-            right_smallest_element[popped_index] = len(heights)
+            area = heights[i] * ((max_distance_on_left + max_distance_on_right)-1)
+            print(max_distance_on_left , max_distance_on_right)
 
-        max_area = -float('inf')
-        for i in range(len(heights)) : 
-            area = heights[i] * ((right_smallest_element[i]-1) - (left_smallest_element[i]+1) + 1)
-            max_area = max(area , max_area)
+            max_area = max(max_area , area)
         
         return max_area
