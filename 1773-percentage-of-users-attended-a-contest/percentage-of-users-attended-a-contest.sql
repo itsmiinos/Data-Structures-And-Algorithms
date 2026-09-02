9a6d617792b46c6fc1f30a6ef0d746426e41e1fe
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-02 21:43:41 +0530

    Time: 1035 ms (61.75%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,2 @@
 # Write your MySQL query statement below
-select contest_id , round(count(user_id) * 100 / (select count(user_id) from users) , 2) as percentage from register group by contest_id order by percentage desc , contest_id asc;
-
-# PySpark :
--- total_user_count = users.count()
--- result = register.groupBy(col("contest_id"))\
---                 .agg(
---                     round(
---                         count(col("user_id")) * 100.00 / total_user_count
---                     ,2)\
---                 .alias("percentage"))\
---                 .orderBy(desc(col("percentage")) , asc(col("contest_id")))
+select contest_id , round((count(user_id) * 100.00 / (select count(user_id) from users)),2) as percentage from register group by contest_id order by percentage desc , contest_id asc;
