5a6085e0affd5229610e6598d915acf296054fab
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-02 22:14:45 +0530

    Time: 100 ms (50.10%) | Memory: 31.8 MB (53.61%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,43 +1,46 @@
-import sys
 class MinStack:
 
     def __init__(self):
-        self.min_stack = []
-        self.min_till_now = sys.maxsize
+        self.my_stack = []
+        self.min_value = float('inf')
 
-    def push(self, val: int) -> None:
-        if len(self.min_stack) == 0 : 
-            self.min_till_now = val
-            diff = val - self.min_till_now
-            self.min_stack.append(diff)
-            
-        else : 
-            diff = val - self.min_till_now
-            self.min_stack.append(diff)
-            if diff < 0 : 
-                self.min_till_now = val
+    def push(self, value: int) -> None:
+        if len(self.my_stack) == 0 :
+            self.min_value = value
+            self.my_stack.append(0)
+        else :
+            diff = value - self.min_value
+            self.my_stack.append(diff)
+
+            if diff < 0 :
+                self.min_value = value
+        
 
     def pop(self) -> None:
-        if len(self.min_stack) == 0 : return
-
-        popped = self.min_stack.pop(-1)
-        if popped < 0 : 
-            self.min_till_now = self.min_till_now - popped
+        if self.my_stack[-1] < 0 :
+            old_value = self.min_value
+            self.min_value = self.min_value - self.my_stack[-1]
+            self.my_stack.pop(-1)
+            return old_value
+        else :
+            value = self.min_value + self.my_stack[-1]
+            self.my_stack.pop(-1)
+            return value
 
     def top(self) -> int:
-        top = self.min_stack[-1]
-        if top < 0 : 
-            return self.min_till_now
-        else : 
-            return self.min_till_now + top
+        print(self.my_stack)
+        if self.my_stack[-1] < 0 :
+            return self.min_value
+        else :
+            return self.min_value + self.my_stack[-1]
 
     def getMin(self) -> int:
-        return self.min_till_now
+        return self.min_value
 
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
-# obj.push(val)
+# obj.push(value)
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
