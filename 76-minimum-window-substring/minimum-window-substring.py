8a65907dfb75326fab6f8dd647de78010f54c4a8
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-17 08:25:44 +0530

    Time: 54 ms (91.06%) | Memory: 19.6 MB (74.13%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,42 +3,40 @@ class Solution:
         if len(t) > len(s) :
             return ""
 
-        t_map = collections.defaultdict(int)
+        freq_t = collections.defaultdict(int)
+        for i in range(len(t)) :
+            freq_t[t[i]] +=1
         
-        for i in range(len(t)):
-            t_map[t[i]] +=1
-
-        i = 0
-        j = 0
-        min_start = -1
+        l = 0
+        r = 0
+        req_count = len(t)
+        count = 0
         min_len = float('inf')
-        s_map = collections.defaultdict(int)
+        min_start = -1
 
-        while j < len(s) :
-            s_map[s[j]] +=1
-
-            while self.s_contains_t(s_map , t_map) :
-                
-                if (j - i + 1) < min_len :
-                    min_start = i
-                    min_len = (j - i + 1)
-                
-                s_map[s[i]]-=1
-                i+=1
+        while r < len(s) :
+            if freq_t[s[r]] > 0 : # this mean we need that char
+                count+=1
             
-            j+=1
+            freq_t[s[r]] -=1
+
+            while count == req_count :
+                if (r - l + 1) < min_len :
+                    min_len = r - l + 1
+                    min_start = l
+                
+                freq_t[s[l]]+=1
+
+                if freq_t[s[l]] > 0 :
+                    count-=1
+                
+                l+=1
+
+            r+=1
         
         if min_start == -1 :
             return ""
-            
+        
         return s[min_start : min_start + min_len]
-
-    def s_contains_t(self , s_map , t_map) -> bool :
-
-        for key in t_map.keys() :
-            if key not in s_map or t_map[key] > s_map[key] :
-                return False
-        
-        return True
-        
-            
+                
+             
