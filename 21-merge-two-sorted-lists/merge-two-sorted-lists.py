c0f0d21b19ca27f77423745f7f551231c3769b19
Yudhajit <yudhajitchowdhury17@gmail.com>
2026-09-22 08:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.13%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,31 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None :
-            return
-        if list1 is None and list2 is not None :
-            return list2
-        if list1 is not None and list2 is None :
-            return list1
-        
-        dummyNode = ListNode(-1)
+    def mergeTwoLists(self, list1: ListNode | None, list2: ListNode | None) -> ListNode | None:
+        curr1 = list1
+        curr2 = list2
+        dummyNode = ListNode()
         temp = dummyNode
 
-        while (list1 and list2) :
-            if list1.val < list2.val :
-                temp.next = list1
-                temp = temp.next
-                list1 = list1.next
+        while curr1 is not None and curr2 is not None :
+            if curr1.val < curr2.val :
+                temp.next = curr1
+                curr1 = curr1.next
+
             else :
-                temp.next = list2
-                temp = temp.next
-                list2 = list2.next
+                temp.next = curr2
+                curr2 = curr2.next
+            
+            temp = temp.next
         
-        if list1 is not None :
-            temp.next = list1
+        while curr1 is not None :
+            temp.next = curr1
+            curr1 = curr1.next
+            temp = temp.next
         
-        else :
-            temp.next = list2
+        while curr2 is not None :
+            temp.next = curr2
+            curr2 = curr2.next
+            temp = temp.next
         
         return dummyNode.next
